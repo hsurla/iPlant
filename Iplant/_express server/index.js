3a30d65b1ee4a3b321a8c79ca4a3dbabf042fca1
NSankalp <101545367+NSankalp@users.noreply.github.com>
2024-03-26 00:05:09 +0530

    updated

---
+++ Iplant/_express server/index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import bodyParser from "body-parser";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const app = express();
+const port = 3000;
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/public/signup.html");
+  });
+
+  app.post("/submit",(req,res)=>{
+    console.log(req.body);
+    res.redirect("/");
+});
+
+
+app.listen(port,()=>{
+    console.log(`running on port ${port}.`);
+})
