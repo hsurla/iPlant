3db005b2b86d0249bc219a13beafdd3b70929173
NSankalp <101545367+NSankalp@users.noreply.github.com>
2024-03-26 15:38:06 +0530

    updated

--- Iplant/_express server/index.js
+++ Iplant/_express server/index.js
@@ -8,18 +8,18 @@ const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(express.static('public')); // Serve static files from the 'public' directory
 
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/public/signup.html");
-  });
-
-  app.post("/submit",(req,res)=>{
-    console.log(req.body);
-    res.redirect("/");
 });
 
+app.post("/submit",(req,res)=>{
+    console.log(req.body);
+    // Redirect to iPlant.html upon form submission
+    res.redirect("/iPlant.html");
+});
 
 app.listen(port,()=>{
     console.log(`running on port ${port}.`);
-})
+});
